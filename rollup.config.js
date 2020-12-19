0806b98dc7e21e5c96444dac569da1f131d1b8d9
dsibi <dasibirin@gmail.com>
2020-12-19 17:30:03 +0300

    add up to 18

--- rollup.config.js
+++ rollup.config.js
@@ -8,31 +8,40 @@ import livereload from 'rollup-plugin-livereload'
 const watch = process.env.ROLLUP_WATCH
 
 export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'umd'
-  },
-  onwarn: function (warning, warn) {
-    if (warning.code === 'CIRCULAR_DEPENDENCY') return;
-    warn(warning);
-  },
-  plugins: [
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    }),
-    resolve({
-      browser: true
-    }),
-    babel({
-      presets: ['@babel/preset-react'],
-      babelHelpers: 'bundled'
-    }),
-    commonjs(),
-    watch && serve('dist'),
-    watch && livereload()
-  ],
-  watch: {
-    clearScreen: false
-  }
+    input: 'src/index.js',
+    output: {
+        file: 'dist/bundle.js',
+        format: 'iife',
+        globals: {
+            'vega': 'vega',
+            'vega-lite': 'vegaLite',
+            'vega-lite-api': 'vl',
+            'vega-tooltip': 'vegaTooltip',
+            'd3': 'd3'
+        }
+    },
+    onwarn: function(warning, warn) {
+        if (warning.code === 'CIRCULAR_DEPENDENCY') return;
+        warn(warning);
+    },
+    plugins: [
+        replace({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        }),
+        resolve({
+            browser: true
+        }),
+        babel({
+            presets: [
+                ['@babel/preset-react', { runtime: 'automatic' }]
+            ],
+            babelHelpers: 'bundled'
+        }),
+        commonjs(),
+        watch && serve('dist'),
+        watch && livereload()
+    ],
+    watch: {
+        clearScreen: false
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,41 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { arc } from 'd3';
-import { BackgroundCircle } from './BackgroundCircle';
+import vega from 'vega';
+import vegaLite from 'vega-lite';
+import vl from 'vega-lite-api';
+import { Handler } from 'vega-tooltip';
+import { config } from './config';
+import { getData } from './getData';
+import { viz } from './viz';
 
-const width = 960;
-const height = 500;
-const centerX = width / 2;
-const centerY = height / 2;
-const strokeWidth = 20;
-const eyeoffsetX = 110;
-const eyeoffsetY = 70;
-const eyeRadius = 40;
-const mouthWidh = 20;
-const mouthRadius = 150;
+vl.register(vega, vegaLite, {
+    view: { renderer: 'svg' },
+    init: view => { view.tooltip(new Handler().call); }
+});
 
-const mouthArc = arc()
-    .innerRadius(mouthRadius)
-    .outerRadius(mouthRadius + mouthWidh)
-    .startAngle(Math.PI / 2)
-    .endAngle(Math.PI * 3 / 2);
+const run = async() => {
+    const marks = viz
+        .data(await getData())
+        .width(window.innerWidth)
+        .height(window.innerHeight)
+        .autosize({ type: 'fit', contains: 'padding' })
+        .config(config);
 
-const App = () => (
-    <svg width={width} height={height}>
-        <g transform={`translate(${centerX},${centerY})`}>
-            <BackgroundCircle radius={centerY - strokeWidth / 2}
-                strokeWidth={strokeWidth} />
-            <circle
-                cx={- eyeoffsetX}
-                cy={- eyeoffsetY}
-                r={eyeRadius} />
-            <circle
-                cx={+ eyeoffsetX}
-                cy={- eyeoffsetY}
-                r={eyeRadius} />
-            <path d={mouthArc()} />
-        </g>
-    </svg>
-);
-
-ReactDOM.render(<App />, document.getElementById('root'));
+    document.body.appendChild(await marks.render());
+};
+run();
